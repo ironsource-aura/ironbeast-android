1d42285faebfa66d6268ef68c7365f4e5dcce856
Michael Iline <michael.iline@ironsrc.com>
2015-12-02 11:58:28 +0200

    Implemented partial doReport, Implemented sendData

--- ironbeastsdk/src/main/java/com/ironsource/mobilcore/IronBeastReportData.java
+++ ironbeastsdk/src/main/java/com/ironsource/mobilcore/IronBeastReportData.java
@@ -3,6 +3,7 @@ package com.ironsource.mobilcore;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,7 +11,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.util.concurrent.TimeUnit;
 
 class IronBeastReportData {
 
@@ -34,10 +34,13 @@ class IronBeastReportData {
     }
 
     public synchronized void doReport(Context context, Intent intent) {
+        Debug.waitForDebugger();
         Logger.log("doReport --->", Logger.SDK_DEBUG);
         try {
             if (intent.getExtras() != null) {
                 int event = intent.getIntExtra(IronBeastReportIntent.EXTRA_REPORT_TYPE, SdkEvent.ERROR);
+                Logger.log("doReport event --> " + event, Logger.SDK_DEBUG);
+
                 // We want to send report
                 // immediately: IRON_BEAST_REPORT isBulk = false
                 // pending : ERROR, IRON_BEAST_REPORT isBulk = true
@@ -63,8 +66,11 @@ class IronBeastReportData {
                     isBulk = true;
                 } else if (event == SdkEvent.ENQUEUE) {
                     //TODO: add something
+                    isBulk = false;
+                } else if (event == SdkEvent.POST_SYNC) {
 
                 } else if (event == SdkEvent.ERROR) {
+                    //TODO: create ERROR report
                     isBulk = true;
                 }
 
@@ -80,12 +86,27 @@ class IronBeastReportData {
                         for (String record : records) {
                             array.put(record);
                         }
+
                         String data = array.toString();
                         String message = createMessage(table, token, data);
                         String encryptedData = Utils.auth(message, token);
-                        SEND_RESULT sendResult = sendData(context, encryptedData, IBConfig.getsInstance().getIBEndPoint());
 
-                        if (sendResult == SEND_RESULT.FAILED) {
+                        SEND_RESULT sendResult = sendData(context, encryptedData, IBConfig.getsInstance().getIBEndPoint());
+                        Logger.log("Send result --> " + sendResult.toString(), Logger.SDK_DEBUG);
+                        if (sendResult == SEND_RESULT.FAILED_DELETE) {
+                            Logger.log("Remove report file --> " + sendResult.toString(), Logger.SDK_DEBUG);
+                            FsQueue.getInstance(table, context).clear();
+                            //TODO: send error report delete reason
+                        } else if (sendResult == SEND_RESULT.FAILED_RESEND_LATER) {
+                            int currNumOfRetry = intent.getExtras().getInt("nRetries");
+                            int idleSeconds = IBConfig.getsInstance().getIdleSeconds();
+                            Logger.log("Resubmit report --> " + currNumOfRetry, Logger.SDK_DEBUG);
+                            //data was save so we need just flush the queue
+                            intent.putExtra(IronBeastReportIntent.EXTRA_REPORT_TYPE, SdkEvent.FLUSH_QUEUE);
+                            //resubmit last intent
+                            resubmitReport(context, intent, currNumOfRetry, idleSeconds);
+                        } else if (sendResult == SEND_RESULT.SUCCESS) {
+                            Logger.log("Remove report file --> " + sendResult.toString(), Logger.SDK_DEBUG);
                             FsQueue.getInstance(table, context).clear();
                         }
                     }
@@ -94,6 +115,22 @@ class IronBeastReportData {
                     //TODO: create message for ironBeast
                     //TODO: encrypt data
                     //TODO: send data to storage or network
+                    String data = dataObject.getString(IronBeastReportIntent.DATA);
+                    String message = createMessage(table, token, data);
+                    String encryptedData = Utils.auth(message, token);
+
+                    SEND_RESULT sendResult = sendData(context, encryptedData, IBConfig.getsInstance().getIBEndPoint());
+                    Logger.log("Send result --> " + sendResult.toString(), Logger.SDK_DEBUG);
+                    if (sendResult == SEND_RESULT.FAILED_RESEND_LATER) {
+                        int currNumOfRetry = intent.getExtras().getInt("nRetries");
+                        int idleSeconds = IBConfig.getsInstance().getIdleSeconds();
+                        Logger.log("Resubmit report --> " + currNumOfRetry, Logger.SDK_DEBUG);
+                        boolean resubmitResult = resubmitReport(context, intent, currNumOfRetry, idleSeconds);
+                        if (!resubmitResult) {
+                            Logger.log("On send failed save report --> ", Logger.SDK_DEBUG);
+                            FsQueue.getInstance(table, context).push(data);
+                        }
+                    }
                 }
 
             }
@@ -120,40 +157,40 @@ class IronBeastReportData {
         SEND_RESULT sendResult = SEND_RESULT.FAILED_RESEND_LATER;
 
         int nRetry = IBConfig.getsInstance().getNumOfRetries();
-        int idleSeconds = IBConfig.getsInstance().getIdleSeconds();
-        while (nRetry-- > 0) {
-            RemoteService poster = HttpService.getInstance();
-            if (poster.isOnline(context)) {
-                try {
-                    RemoteService.Response response = poster.post(encryptedData, ibEndPoint);
-                    if (response.code == HttpURLConnection.HTTP_OK) {
-                        sendResult = SEND_RESULT.SUCCESS;
-                    }
-                    //TODO: Check internal error from what android version
-
-                    if(response.code >= HttpURLConnection.HTTP_BAD_REQUEST && response.code < HttpURLConnection.HTTP_INTERNAL_ERROR) {
-                        sendResult = SEND_RESULT.FAILED;
-                        break;
-                        //clear data
-                    } else if(response.code >= HttpURLConnection.HTTP_INTERNAL_ERROR && response.code < HttpURLConnection.HTTP_VERSION) {
-                        sendResult = SEND_RESULT.FAILED_RESEND_LATER;
-                    }
-                    // TODO: Handle 40X, 50x status situations
-                } catch (IOException e) {
-                    Logger.log("Failed to Post to Ironbeast", Logger.SDK_DEBUG);
-                }
-            }
+        RemoteService poster = HttpService.getInstance();
+        if (poster.isOnline(context)) {
             try {
-                TimeUnit.SECONDS.sleep(idleSeconds);
-            } catch (InterruptedException e) {
-                Logger.log("Failed to idle", Logger.SDK_DEBUG);
+                RemoteService.Response response = poster.post(encryptedData, ibEndPoint);
+                if (response.code == HttpURLConnection.HTTP_OK) {
+                    sendResult = SEND_RESULT.SUCCESS;
+                }
+                //TODO: Check ״internal error״ from what android version
+                if (response.code >= HttpURLConnection.HTTP_BAD_REQUEST && response.code < HttpURLConnection.HTTP_INTERNAL_ERROR) {
+                    sendResult = SEND_RESULT.FAILED_DELETE;
+                } else if (response.code >= HttpURLConnection.HTTP_INTERNAL_ERROR && response.code < HttpURLConnection.HTTP_VERSION) {
+                    sendResult = SEND_RESULT.FAILED_RESEND_LATER;
+                }
+                // TODO: Handle 40X, 50x status situations
+            } catch (IOException e) {
+                Logger.log("Failed to Post to Ironbeast", Logger.SDK_DEBUG);
             }
         }
 
         return sendResult;
     }
+
+    boolean resubmitReport(Context context, Intent intent, int currNumOfRetry, long delay) {
+        boolean result = false;
+        if (0 <= currNumOfRetry && currNumOfRetry < IBConfig.getsInstance().getNumOfRetries()) {
+            intent.getExtras().putInt("nRetries", currNumOfRetry++);
+            Utils.scheduleSendReportsAction(context, intent, delay);
+            result = true;
+        }
+        return result;
+    }
+
     enum SEND_RESULT {
-        SUCCESS, FAILED, FAILED_RESEND_LATER
+        SUCCESS, FAILED_DELETE, FAILED_RESEND_LATER
     }
     private static final int SEND_RESU_EVENTS = 1; // Put message in the QStorage
     private static final int FLUSH_QUEUE = 2;    // Flush QStorage
